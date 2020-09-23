2c8ef9c4531ac294d986c409b131869ff8cf6e3e
Eric Grant <senatorericgrant@gmail.com>
2020-09-24 00:36:39 +0700

    feat: create input Container View

--- AutomobileCatalog/LoginController.swift
+++ AutomobileCatalog/LoginController.swift
@@ -20,6 +20,29 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let image = UIImage(systemName: "envelope.badge", withConfiguration: UIImage.SymbolConfiguration(weight: .medium))
+        let view = Utillities().inputContainerView(withImage: image!, textField: emailTextField)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let image = UIImage(systemName: "lock", withConfiguration: UIImage.SymbolConfiguration(weight: .medium))
+        let view = Utillities().inputContainerView(withImage: image!, textField: passwordTextField)
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utillities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utillities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -40,5 +63,17 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor,
+                     left: view.leftAnchor,
+                     right: view.rightAnchor,
+                     paddingLeft: 32,
+                     paddingRight: 32)
     }
 }
